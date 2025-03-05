60e047a64d91246437571d329f7bacf5ea94b033
Sumit Abhishek <sumitabhi1997@gmail.com>
2025-03-05 21:15:10 +0530

    Adding Error RTK Query API to the client

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -3,15 +3,17 @@ import { counterSlice } from "../../features/contact/contactReducer";
 import { useDispatch, useSelector } from "react-redux";
 import { catalogApi } from "../../features/catalog/catalogApi";
 import { uiSlice } from "../layout/uiSlice";
+import { errorApi } from "../../features/about/errorApi";
 
 export const store = configureStore({
   reducer: {
     [catalogApi.reducerPath]: catalogApi.reducer,
+    [errorApi.reducerPath]: errorApi.reducer,
     counter: counterSlice.reducer,
     ui: uiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(catalogApi.middleware),
+    getDefaultMiddleware().concat(catalogApi.middleware, errorApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,6 +1,58 @@
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import {
+  useLazyGet400ErrorQuery,
+  useLazyGet401ErrorQuery,
+  useLazyGet404ErrorQuery,
+  useLazyGet500ErrorQuery,
+  useLazyGetValidationErrorQuery,
+} from "./errorApi";
+
 const AboutPage = () => {
+  const [trigger400Error] = useLazyGet400ErrorQuery();
+  const [trigger401Error] = useLazyGet401ErrorQuery();
+  const [trigger404Error] = useLazyGet404ErrorQuery();
+  const [trigger500Error] = useLazyGet500ErrorQuery();
+  const [triggerValidationError] = useLazyGetValidationErrorQuery();
   return (
-    <div>AboutPage</div>
-  )
-}
-export default AboutPage
+    <Container maxWidth="lg">
+      <Typography gutterBottom variant="h3">
+        Errors for Testing
+      </Typography>
+      <ButtonGroup fullWidth>
+        <Button
+          variant="contained"
+          onClick={() => trigger400Error().catch((err) => console.log(err))}
+        >
+          Test 400 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => trigger401Error().catch((err) => console.log(err))}
+        >
+          Test 401 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => trigger404Error().catch((err) => console.log(err))}
+        >
+          Test 404 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => trigger500Error().catch((err) => console.log(err))}
+        >
+          Test 500 Error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() =>
+            triggerValidationError().catch((err) => console.log(err))
+          }
+        >
+          Test Validation Error
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
+};
+export default AboutPage;
