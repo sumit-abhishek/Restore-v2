376c8eb129d4ca7f1854db9673afc8294a2e6237
Sumit Abhishek <sumitabhi1997@gmail.com>
2025-03-06 23:16:29 +0530

    Create Toast For Validation Errors

--- client/src/app/api/baseApi.ts
+++ client/src/app/api/baseApi.ts
@@ -4,11 +4,12 @@ import {
   fetchBaseQuery,
 } from "@reduxjs/toolkit/query";
 import { startLoading, stopLoading } from "../layout/uiSlice";
+import { toast } from "react-toastify";
 
 const customBaseQuery = fetchBaseQuery({
   baseUrl: "https://localhost:5001/api",
 });
-
+type ErrorResponse = string | { title: string } | { errors: string };
 const sleep = () => new Promise((resolve) => setTimeout(resolve, 1000));
 
 export const baseQueryWithErrorHandling = async (
@@ -21,8 +22,33 @@ export const baseQueryWithErrorHandling = async (
   const result = await customBaseQuery(args, api, extraOptions);
   api.dispatch(stopLoading());
   if (result.error) {
-    const { status, data } = result.error;
-    console.log({ status, data });
+    const originalStatus =
+      result.error.status === "PARSING_ERROR" && result.error.originalStatus
+        ? result.error.originalStatus
+        : result.error.status;
+    const responseData = result.error.data as ErrorResponse;
+    switch (originalStatus) {
+      case 400:
+        if (typeof responseData === "string") toast.error(responseData);
+        else if ("errors" in responseData) {
+          toast.error("Validation Error");
+        } else toast.error(responseData.title);
+        break;
+      case 401:
+        if (typeof responseData === "object" && "title" in responseData)
+          toast.error(responseData.title);
+        break;
+      case 404:
+        if (typeof responseData === "object" && "title" in responseData)
+          toast.error(responseData.title);
+        break;
+      case 500:
+        if (typeof responseData === "object" && "title" in responseData)
+          toast.error(responseData.title);
+        break;
+      default:
+        break;
+    }
   }
   return result;
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,9 +9,12 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes/Routes.tsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store/store.ts";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
