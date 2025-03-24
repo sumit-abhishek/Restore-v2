a500f778e8400d0a13d2ebb432f5166c262d2909
Sumit Abhishek <sumitabhi1997@gmail.com>
2025-03-24 20:47:29 +0530

    Creted Extension for adding generic methods

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -2,6 +2,7 @@ using System;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,22 +16,12 @@ public class BasketController(StoreContext context) : BaseApiController
         var basket = await RetrieveBasket();  
 
         if (basket == null) NoContent();
-        return new BasketDto{
-            BasketId=basket.BasketId,
-            Items=basket.Items.Select(x=>new BasketItemDto{
-                ProductId=x.ProductId,
-                Name=x.Product.Name,
-                Price=x.Product.Price,
-                Brand=x.Product.Brand,
-                Type=x.Product.Type,
-                PictureUrl=x.Product.PictureUrl,
-                Quantity=x.Quantity
-            }).ToList()
-        };
+
+        return basket.ToDto();
     }
 
     [HttpPost]
-    public async Task<ActionResult>AddItemToBasket(int productId, int quantity)
+    public async Task<ActionResult<BasketDto>>AddItemToBasket(int productId, int quantity)
     {
         var basket =await RetrieveBasket();   //Get Basket
         
@@ -42,7 +33,7 @@ public class BasketController(StoreContext context) : BaseApiController
         basket.AddItem(product,quantity); //Add Product
 
         var result= await context.SaveChangesAsync()>0;  //Save Changes
-        if(result) return CreatedAtAction(nameof(GetBasket),basket);
+        if(result) return CreatedAtAction(nameof(GetBasket),basket.ToDto());
 
         return BadRequest("Problem Updating Basket");
     }
