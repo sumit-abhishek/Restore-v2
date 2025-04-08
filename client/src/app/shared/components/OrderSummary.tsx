713703218cc54169f23b3798784ff6daa8bc4643
Sumit Abhishek <sumitabhi1997@gmail.com>
2025-04-08 07:32:39 +0530

    Added Order Summary Functionality

--- client/src/app/shared/components/OrderSummary.tsx
+++ client/src/app/shared/components/OrderSummary.tsx
@@ -7,10 +7,17 @@ import {
   Paper,
 } from "@mui/material";
 import { currencyFormat } from "../../../lib/util";
+import { useFetchBasketQuery } from "../../../features/basket/basketApi";
+import { Item } from "../../models/basket";
 
 export default function OrderSummary() {
-  const subtotal = 0;
-  const deliveryFee = 0;
+  const { data: basket } = useFetchBasketQuery();
+  const subtotal =
+    basket?.items.reduce(
+      (sum: number, item: Item) => sum + item.quantity * item.price,
+      0
+    ) ?? 0;
+  const deliveryFee = subtotal > 1000 ? 0 : 500;
 
   return (
     <Box
