4f97a0b9bccb7860827737d3e2908b9faf0c3338
Sumit Abhishek <sumitabhi1997@gmail.com>
2025-04-26 16:24:44 +0530

    Setting up pagination on the client

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,20 +1,36 @@
-import { Grid2 } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
 import { useFetchProductsQuery } from "./catalogApi";
 import ProductList from "./ProductList";
 import Filters from "./Filters";
-import { useAppSelector } from "../../app/store/store";
+import { useAppDispatch, useAppSelector } from "../../app/store/store";
+import AppPagination from "../../app/shared/components/AppPagination";
+import { setPageNumber } from "./catalogSlice";
 
 const Catalog = () => {
   const productParams = useAppSelector((state) => state.catalog);
   const { data, isLoading } = useFetchProductsQuery(productParams);
+  const dispatch = useAppDispatch();
   if (isLoading || !data) return <div>Loading...</div>;
+
   return (
     <Grid2 container spacing={4}>
       <Grid2 size={3}>
         <Filters />
       </Grid2>
       <Grid2 size={9}>
-        <ProductList products={data} />
+        {data.items && data.items.length > 0 ? (
+          <>
+            <ProductList products={data.items} />
+            <AppPagination
+              metadata={data.pagination}
+              onPageChange={(page: number) => dispatch(setPageNumber(page))}
+            />
+          </>
+        ) : (
+          <Typography variant="h5">
+            There are no result for this filter
+          </Typography>
+        )}
       </Grid2>
     </Grid2>
   );
--- client/src/features/catalog/catalogApi.ts
+++ client/src/features/catalog/catalogApi.ts
@@ -3,17 +3,28 @@ import { Product } from "../../app/models/products";
 import { baseQueryWithErrorHandling } from "../../app/api/baseApi";
 import { ProductParams } from "../../app/models/productParams";
 import { filterEmptyValues } from "../../lib/util";
+import { Pagination } from "../../app/models/pagination";
 
 export const catalogApi = createApi({
   reducerPath: "catalogApi",
   baseQuery: baseQueryWithErrorHandling,
   endpoints: (builder) => ({
-    fetchProducts: builder.query<Product[], ProductParams>({
+    fetchProducts: builder.query<
+      { items: Product[]; pagination: Pagination },
+      ProductParams
+    >({
       query: (productParams) => {
         return {
           url: "products",
           params: filterEmptyValues(productParams),
         };
+      },
+      transformResponse: (items: Product[], meta) => {
+        const paginationHeader = meta?.response?.headers.get("Pagination");
+        const pagination = paginationHeader
+          ? JSON.parse(paginationHeader)
+          : null;
+        return { items, pagination };
       },
     }),
     fetchProductDetails: builder.query<Product, number>({
