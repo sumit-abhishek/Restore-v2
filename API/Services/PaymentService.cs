e7977dbfbe961543ef56c7c6b4f9d941ee44fbcf
Sumit Abhishek <sumitabhi1997@gmail.com>
2025-06-11 22:33:00 +0530

    Creating a payments service

---
+++ API/Services/PaymentService.cs
@@ -0,0 +1,39 @@
+using System;
+using API.Entities;
+using Stripe;
+
+namespace API.Services;
+
+public class PaymentService(IConfiguration config)
+{
+    public async Task<PaymentIntent> CreateOrUpdatePaymentIntent(Basket basket)
+    {
+        StripeConfiguration.ApiKey = config["StripeSetting:SecretKey"];
+        var service = new PaymentIntentService();
+        var intent = new PaymentIntent();
+        var subtotal = basket.Items.Sum(x => x.Quantity * x.Product.Price);
+        var deliveryFee = subtotal > 10000 ? 0 : 500;
+
+        if (string.IsNullOrEmpty(basket.PaymentIntentId))
+        {
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = subtotal + deliveryFee,
+                Currency = "usd",
+                PaymentMethodTypes = ["card"]
+            };
+            intent = await service.CreateAsync(options);
+        }
+        else
+        {
+            var options = new PaymentIntentUpdateOptions
+            {
+                Amount = subtotal + deliveryFee,
+            };
+            await service.UpdateAsync(basket.PaymentIntentId, options);
+        }
+        return intent;
+
+    }
+
+}
